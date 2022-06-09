ac496facfa357de70426a9e80a9e07e9cba8525a
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-06-09 17:59:20 -0600

    Add files

--- friends.js
+++ friends.js
@@ -3,5 +3,9 @@ var numbers = [1, 2, 3, 4, 5, 6, 7, 99]
 for (var i = 0; i < numbers.length; i++) {
   if (numbers[i] > 30) {
     console.log(numbers[i]);
+  } else if (numbers[i] < 60) {
+    // code to execute if condition number 2 evaluates to true
+    console.log("Numbers under 60 " + numbers[i])
   }
+
 }
